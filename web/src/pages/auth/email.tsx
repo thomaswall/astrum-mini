12bb05e89abaeb3962af7598a5be9ae91893abb8
Dax Ravi <d@ironbay.co>
2020-05-13 15:25:46 -0400

    Remove form functionality

--- web/src/pages/auth/email.tsx
+++ web/src/pages/auth/email.tsx
@@ -15,7 +15,7 @@ export default function Email(props: RouteComponentProps) {
             Sign in to your account
           </h2>
           <p className="mt-2 text-center text-sm leading-5 text-gray-600">
-            Or &nbsp;
+            Or&nbsp;
             <a
               href="#"
               className="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150"
@@ -24,16 +24,14 @@ export default function Email(props: RouteComponentProps) {
             </a>
           </p>
         </div>
-        <form className="mt-8" action="#" method="POST">
-          <input type="hidden" name="remember" value="true" />
+        <div className="mt-8">
           <div className="rounded-md shadow-sm">
             <div>
-              {" "}
               Email Address
               <input
                 aria-label="Email address"
                 name="email"
-                type="email"
+                type="input"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 sm:text-sm sm:leading-5"
                 placeholder="Email address"
@@ -42,8 +40,7 @@ export default function Email(props: RouteComponentProps) {
           </div>
           <div className="mt-6">
             <button
-              onClick={() => navigate(`/confirm`)}
-              type="submit"
+              onClick={() => navigate(`/auth/confirm`)}
               className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
             >
               <span className="absolute left-0 inset-y-0 flex items-center pl-3">
@@ -62,7 +59,7 @@ export default function Email(props: RouteComponentProps) {
               Sign in
             </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   )
