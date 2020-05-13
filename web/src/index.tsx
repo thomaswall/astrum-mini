e46ad21a395d2f4b4df59e42f507d8c7ded7682e
Cienna Gray <ciennagray@ciennas-mbp.home>
2020-05-13 12:38:35 -0400

    adding passwordless sign in page

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,12 +5,16 @@ import ReactDOM from "react-dom"
 
 import Home from "./pages/home"
 import About from "./pages/about"
+import Email from "./pages/auth/email"
+import Confirm from "./pages/auth/confirm"
 
 function App() {
   return (
     <Router>
       <Home path="/" />
       <About path="/about" />
+      <Email path="/auth/email" />
+      <Confirm path="/auth/confirm"/>
     </Router>
   )
 }
