56f5caee4a9da3dd8f475f57b3b127e7cb9d2f72
Tom Wall <thomasjwall1@gmail.com>
2020-05-16 17:13:42 -0700

    rt 1

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { local, sync } from "../data/riptide";
 import { RouteComponentProps } from "@reach/router"
 
 export default function Home(props: RouteComponentProps) {
